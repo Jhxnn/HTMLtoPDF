5ebaa7859fe8b7590a87fef1c5cb810586727019
Hilixx <jhonalizan@gmail.com>
2025-05-12 21:44:05 -0300

    feature: finalizdo

--- src/main/java/com/HTMLToPdf/controllers/ConvertController.java
+++ src/main/java/com/HTMLToPdf/controllers/ConvertController.java
@@ -1,12 +1,10 @@
 package com.HTMLToPdf.controllers;
 
 
-import com.HTMLToPdf.dtos.FileDto;
 import com.HTMLToPdf.services.ConvertService;
 import com.HTMLToPdf.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,13 +22,14 @@ public class ConvertController {
     ConvertService convertService;
 
     @GetMapping
-    public ResponseEntity<String> enviarEmail(@RequestBody FileDto fileDto) throws IOException {
-        byte[] pdf = convertService.convertHtmlFileToPdf(fileDto.file());
-        emailService.enviarEmailComPdf(fileDto.email(),
+    public ResponseEntity<String> enviarEmail(@RequestParam("file") MultipartFile file,
+                                              @RequestParam("email") String email) throws IOException {
+        byte[] pdf = convertService.convertHtmlFileToPdf(file);
+        emailService.enviarEmailComPdf(email,
                 "Registro de Ponto",
                 "Segue registro de ponto em PDF",
                 pdf,
                 "registro");
-        return ResponseEntity.status(HttpStatus.OK).body("Arquivo convertido e enviado para: " + fileDto.email());
+        return ResponseEntity.status(HttpStatus.OK).body("Arquivo convertido e enviado para: " + email);
     }
 }
--- src/main/java/com/HTMLToPdf/dtos/FileDto.java
+++
@@ -1,6 +0,0 @@
-package com.HTMLToPdf.dtos;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public record FileDto(MultipartFile file, String email) {
-}
